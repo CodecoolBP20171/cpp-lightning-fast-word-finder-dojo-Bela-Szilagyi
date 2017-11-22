1f635f2677556c0c523358d3f4bdf351250a215f
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-11-22 10:05:47 +0100

    grrr

--- LightningWordFinder.hpp
+++ LightningWordFinder.hpp
@@ -4,12 +4,42 @@
 
 #ifndef CPP_LIGHTNING_FAST_WORD_FINDER_DOJO_LIGHTNINGWORDFINDER_HPP
 #define CPP_LIGHTNING_FAST_WORD_FINDER_DOJO_LIGHTNINGWORDFINDER_HPP
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
 class LightningWordFinder
 {
 public:
-    std::string getLongestWord(const std::string& text)
-    {
-        return "Longest Word";
+    std::string getLongestWord(const std::string& text) {
+        std::istringstream iss(text);
+        std::vector<std::string> words{std::istream_iterator<std::string>{iss},
+                                       std::istream_iterator<std::string>{}};
+        //for(auto& word : words) std::cout << word << std::endl;
+        std::sort(
+                words.begin(),
+                words.end(),
+                [](const std::string &a, const std::string &b) {
+                    return a.size() > b.size() || a.size() == b.size() && a > b;
+                });
+        for (auto &word : words) {
+            bool ok = true;
+            for (auto &c : word) {
+                if (!isalpha(c)) {
+                    ok = false;
+                }
+            }
+            if (ok) {
+                //std::cout << word << std::endl;
+                return word;
+            }
+        }
     }
 };
+
 #endif //CPP_LIGHTNING_FAST_WORD_FINDER_DOJO_LIGHTNINGWORDFINDER_HPP
+
+//Average Running Time: 2940723.000000ns
